83e0d5ec13fde400ee2dcc70c17182ae87e1264c
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-11-23 22:02:12 +0300

    modified:colbac.js new file:primer.js

---
+++ primer.js
@@ -0,0 +1,22 @@
+/*
+boolean(value)-> false false
+          0
+           ' '
+            undefined
+               null
+
+let b = 10;
+b && console.log("выполнено");
+
+let c;
+c && console.log("выполнено");
+*/
+const button = {
+  width: 200,
+  text: "buy",
+};
+const redButton = {
+  ...button,
+  color: "red",
+};
+console.table(redButton);
