947e6961f7430cb3c5a9a3768857a1ca9b6499dc
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-11-27 20:47:04 +0300

    new file:manual.jsm

---
+++ manual.js
@@ -0,0 +1,21 @@
+// инструкции
+let a = 10;
+const b = 5;
+if (a > b) {
+  console.log("a is larger");
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+// Выражение-инструкция
+
+let d;
+("abc");
+a = a + 3;
+c = a + b;
+d = "G00D " + "EVENING";
+myFunction(c, d);
+console.log("Hey");
+
+
